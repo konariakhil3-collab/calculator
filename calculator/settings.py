77a93259dbc1b8956110e20f7a723779eaf2b039
konariakhil3-collab <konariakhil3@gmail.com>
2026-01-05 11:54:18 +0530

    Update settings.py

--- calculator/settings.py
+++ calculator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*s*p-_y=5@63)(z)ezgue4hui(m68vq@)()naby=%+eyveb(c-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
